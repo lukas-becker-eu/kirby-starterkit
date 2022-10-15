eb6d7ff32f4140f00105d6201dbd3441a80114e5
Lukas Becker <lukas@lukas-becker.eu>
2022-10-16 00:29:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,9 @@
 
 require __DIR__ . '/kirby/bootstrap.php';
 
-echo (new Kirby)->render();
+echo (new Kirby([
+    'server' => [
+        'SERVER_NAME' => $_SERVER['HTTP_DISGUISED_HOST'],
+        'HTTPS' => $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https'
+    ]
+]))->render();
